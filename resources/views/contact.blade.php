1b583f50cbbbfea58b664362639f0f657703c176
sWARNAV <swarnavsarkar20@protonmail.com>
2026-05-22 14:56:36 +0530

    First commit, add: home, about, contact page

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+    <h1>This is where you contact me.As my other half would say, "I won't provide the details go find it yourself".</h1>
+    <a href="SikeGotYouTwice"><small>Just kidding, here it is.</small></a>
+    </div>
+
+    <hr>
+    
+    <div>
+        <a href="/">Get Back to the Base, Sergeant!</a>
+    </div>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>Hey Jude</h1>
+
+    <div>
+        <ul>
+            <li><a href="/about">The About</a></li>
+            <li><a href="/contact">The Contacts</a></li>
+        </ul>
+
+    </div>
+
+
+</body>
+</html>
