b3f272e94d48f7dacc2a4b4048eaf7f69be0750b
sWARNAV <swarnavsarkar20@protonmail.com>
2026-08-14 15:00:06 +0530

    Minor change to use IdeaRequest for Request

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\IdeaRequest;
 use App\Models\Idea;
 use Illuminate\Http\Request;
 
@@ -36,13 +37,14 @@ class IdeaController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(IdeaRequest $request)
     {
         //This $request parses the entire POST data. 
 
-        $request->validate([
-            'description' => ['required', 'min:10']
-        ]);
+        // $request->validate([
+        //     'description' => ['required', 'min:10']
+        // ]);
+
         Idea::create([
             'description' => $request['description'],
             'state' => "pending"
@@ -80,7 +82,7 @@ class IdeaController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Idea $idea)
+    public function update(IdeaRequest $request, Idea $idea)
     {
         $idea->update([
             'description' => request('description')
